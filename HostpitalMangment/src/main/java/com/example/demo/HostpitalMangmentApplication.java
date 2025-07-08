8162c2fb1dbe2a14a677b0ce59ed893f1209bcd7
admin@avsoftechnologies.com <BXLinx!6>
2025-07-08 08:52:47 +0530

    first commit

---
+++ HostpitalMangment/src/main/java/com/example/demo/HostpitalMangmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostpitalMangmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostpitalMangmentApplication.class, args);
+	}
+
+}
